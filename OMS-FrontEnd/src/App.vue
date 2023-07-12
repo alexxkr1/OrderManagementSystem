43ed72d745ce0cf77fdcd9450b1d7cf83f7f2388
Alexxkr1 <alexandroskrenstrauch@gmail.com>
2023-07-12 19:28:15 +0300

    Frontend initial setup

--- OMS-FrontEnd/src/App.vue
+++ OMS-FrontEnd/src/App.vue
@@ -1,30 +1,43 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <component :is="layout">
+    <Toast/>
+    <RouterView />
+  </component>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const layout = computed(() => {
+  const routeMeta = useRoute().meta
+
+  return routeMeta.layout + '-layout'
+})
+
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  position: relative;
+  font-weight: normal;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body {
+  min-width: 100%;
+  color: #2e3c2d;
+  background-color: #e5e5e5;
+  min-height: 100vh;
+  transition: color 0.5s, #fff 0.5s;
+  line-height: 1.6;
+  font-family: Manrope, sans-serif;
+  font-size: 15px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
--- OMS-FrontEnd/src/main.ts
+++ OMS-FrontEnd/src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import { primevueConfig } from "./primevue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+primevueConfig(app);
+app.use(createPinia());
+app.use(PrimeVue);
+app.use(router);
+
+app.mount("#app");
